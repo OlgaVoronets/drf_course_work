4e35bbad84aac1bd7c59a555299c0703b8871831
olga voronets <voronets80@gmail.com>
2024-03-11 18:36:44 +0300

    Добавлены базовый сериализатор и контроллеры по CRUD для модели привычки

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from habits.models import Habit
+from habits.serializers import HabitSerializer
+
+
+class HabitCreateView(generics.CreateAPIView):
+    """Контроллер создания привычки"""
+    serializer_class = HabitSerializer
+
+
+class HabitDetailView(generics.RetrieveAPIView):
+    """Контроллер просмотра привычки"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitUpdateView(generics.UpdateAPIView):
+    """Контроллер редактирования привычки"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitDeleteView(generics.DestroyAPIView):
+    """Контроллер удаления привычки"""
+    queryset = Habit.objects.all()
+
+
+class HabitListView(generics.ListAPIView):
+    """Контроллер просмотра списка привычек"""
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
