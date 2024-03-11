774ae0dc70e0e43bf8e8e3d642e3f15a21ffecd5
olga voronets <voronets80@gmail.com>
2024-03-11 16:09:35 +0300

    Созданы модель пользователя, и контроллер для регистрации

--- users/models.py
+++ users/models.py
@@ -1,5 +1,16 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Почта')
+    telegram_id = models.CharField(max_length=30, verbose_name='Телеграм', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
